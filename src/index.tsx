c206cb26459a9ff85f1fdfc21eb1d873429841d6
Harshal Bhangale <buddyharshal2751@gmail.com>
2024-10-27 07:34:00 +0530

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+
+// Wallet Integration Imports
+import { http } from 'viem';
+import { WagmiProvider, createConfig } from '@privy-io/wagmi';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { PrivyProvider } from '@privy-io/react-auth';
+import { baseSepolia } from 'viem/chains';
+import type { PrivyClientConfig } from '@privy-io/react-auth';
+import store from "./store/store";
+import { Provider } from 'react-redux';
+
+//  Chain Configuration
+
+
+export const wagmiConfig = createConfig({
+  chains: [baseSepolia],
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+});
+
+const privyConfig: PrivyClientConfig = {
+  appearance: {
+    walletList: ['coinbase_wallet'],
+  },
+  externalWallets: {
+    coinbaseWallet: {
+      // Valid connection options include 'eoaOnly' (default), 'smartWalletOnly', or 'all'
+      connectionOptions: 'smartWalletOnly',
+    },
+  },
+  embeddedWallets: {
+    createOnLogin: 'users-without-wallets',
+    requireUserPasswordOnCreate: true,
+    noPromptOnSignature: false,
+  },
+  defaultChain: baseSepolia,
+  loginMethods: ['wallet', 'email', 'sms'],
+};
+
+// Setup queryClient
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <PrivyProvider
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      appId='cm2qign6s03s3mqfk6dbz71q1'
+      config={privyConfig}
+    >
+      <QueryClientProvider client={queryClient}>
+        <WagmiProvider config={wagmiConfig}>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </WagmiProvider>
+      </QueryClientProvider>
+    </PrivyProvider>
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
